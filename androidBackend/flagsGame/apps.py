00b4f16192f5aba85eb61798fe87e92f4fe465ee
Joe <kellyj40@tcd.ie>
2017-11-05 18:04:24 +0000

    set up basic django

---
+++ androidBackend/flagsGame/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FlagsgameConfig(AppConfig):
+    name = 'flagsGame'
---
+++ androidBackend/flagsGame/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def allFlags():
+    all_flags = {"positions": [[53.267685, -6.119695],
+                 [53.268288, -6.115382],
+                 [53.270912, -6.120339],
+                 [53.269751, -6.121927],
+                 [53.308807, -6.215731],
+                 [53.309126, -6.219639],
+                 [53.307005, -6.222331]]}
+    return all_flags
+
